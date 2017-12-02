0be538956bd0de43f03c45cc6c9b7146dafc74da
vnyk <uservks@gmail.com>
2017-12-02 11:22:25 +0530

    commit without wifi

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-ECHO is on.
+import os, sys
+from flask import flask, request
+
+from pymessenger import Bot
+
+
+access_tkn="wifi chalao bhai"
+bot=Bot(access_tkn);
+
+@app.route("/", methods=['GET'])
+def verify():
+	#verify webhook koi wifi chalado!!!!!!!!!!!
+	if request.args.get("hub.mode")=="subscribe" and request.args.get("hub.challenge"):
+		if not request.args.get("hub.verify_token") == "wifi":
+			return "verify token failed", 
